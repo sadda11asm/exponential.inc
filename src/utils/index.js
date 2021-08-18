5b00abf9fe4cd0df1dd4742448232808c33adb34
Daulet Amirkhanov <damirkhanov01@gmail.com>
2021-08-18 22:55:08 +0000

    hovering button

---
+++ src/utils/index.js
@@ -0,0 +1,23 @@
+import { useRef, useEffect, useState } from 'react';
+import { throttle } from 'lodash';
+
+export function useVisibility(offset = 0, throttleMilliseconds = 100) {
+    const [isVisible, setIsVisible] = useState(false);
+    const currentElement = useRef();
+    
+    const onScroll = throttle(() => {
+        if (!currentElement.current) {
+          setIsVisible(false);
+          return;
+        }
+        const top = currentElement.current.getBoundingClientRect().top;
+        setIsVisible(top + offset >= 0 && top - offset <= window.innerHeight);
+      }, throttleMilliseconds);
+
+      useEffect(() => {
+        document.addEventListener('scroll', onScroll, true);
+        return () => document.removeEventListener('scroll', onScroll, true);
+      });
+    
+      return [isVisible, currentElement];
+}
