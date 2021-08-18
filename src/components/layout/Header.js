746697c642970d03e7a25f797733042aea659159
Daulet Amirkhanov <damirkhanov01@gmail.com>
2021-08-18 22:15:09 +0000

    header text change and mobile css

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,19 +8,19 @@ const Header = () => (
   <header className="font-display sticky top-0 bg-white bg-opacity-10 shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
-        <div className="w-24 mr-6">
+        <div className="w-24">
           {/*<img src={logo} alt="Logo" />*/}
-          <div className="text-primary font-bold font-display text-sm">
+          <div className="text-primary font-bold font-display text-sm text-center">
             YCGI
           </div>
         </div>
       </div>
       <div className="flex mt-4 sm:mt-0">
         <AnchorLink className="px-4" href="#what-we-offer">
-          Идея
+          Программа
         </AnchorLink>
         <AnchorLink className="px-4" href="#program">
-          Программа
+          Процесс
         </AnchorLink>
         <AnchorLink className="px-4" href="#mentors">
           Ментора
