628119ce781bbbc29706daae11fcda74df24e810
Daulet Amirkhanov <damirkhanov01@gmail.com>
2021-08-22 19:37:06 +0000

    console log removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,6 @@ import { Link } from 'gatsby';
 
 const Index = () => {
   const [ topmostSectionIsVisible, topmostSection ] = useVisibility(100);
-  console.log('topmostSectionIsVisible', topmostSectionIsVisible)
 
   return (
     <Layout>
