559dbf4112920dfbdba28772b8cdaf0af10d1e6c
Daulet Amirkhanov <damirkhanov01@gmail.com>
2022-03-23 23:05:22 +0000

    blue line in mentorCards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Card = ({ className, children }) => (
   <div
-    className={`p-12 rounded-lg border border-solid border-gray-200 ${className}`}
+    className={`p-12 rounded-lg border border-solid border-gray-200 ${className} relative overflow-hidden`}
     style={{
       boxShadow: '0 10px 28px rgba(0,0,0,.08)'
     }}
   >
     {children}
+    <div className="absolute inset-x-0 bottom-0 w-full h-2" style={{ backgroundColor: '#166EFF' }}></div>
   </div>
 );
 
