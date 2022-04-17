4457d99ae2e02ac523ff2141ef37aa68de3ff5e1
Daulet Amirkhanov <damirkhanov01@gmail.com>
2022-04-17 00:16:35 +0000

    How it works MVP

--- src/components/how-it-works/index.js
+++ src/components/how-it-works/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import HowItWorksSteps from '../../../assets/how_it_works_steps.svg';
+import LargeHowItWorksSteps from '../../../assets/large_how_it_works_steps.svg';
+import MobileHowItWorksSteps from '../../../assets/mobile_how_it_works_steps.svg';
 import YoutubeIcon from '../../../assets/youtube_icon.svg';
 
 const HowItWorks = () => {
     return (
         <>
-            <h2 className="lg:text-5xl">How does it <span style={{ color: '#2778FD' }}>work?</span></h2>
+            <h2 className="lg:text-5xl sm:text-4xl">How does it <span style={{ color: '#2778FD' }}>work?</span></h2>
             <Link to="" target="_blank"><span className="inline-block" style={{ color: '#2778FD' }}>Watch video</span> <YoutubeIcon className="inline-block"/> </Link>
-            <HowItWorksSteps className="mx-auto mt-8"/>
+            <div className="mt-8">
+                { window.innerWidth >= 1536 ? <LargeHowItWorksSteps className="mx-auto"/> : <MobileHowItWorksSteps className="mx-auto"/> }
+            </div>
         </>
     );
 };
