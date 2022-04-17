e56197162bd995be57dc97a469e127fce5613d9a
Daulet Amirkhanov <damirkhanov01@gmail.com>
2022-04-17 00:25:22 +0000

    chore: added dynamic svg resize to How it Works

--- src/components/how-it-works/index.js
+++ src/components/how-it-works/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 
 import LargeHowItWorksSteps from '../../../assets/large_how_it_works_steps.svg';
 import MobileHowItWorksSteps from '../../../assets/mobile_how_it_works_steps.svg';
 import YoutubeIcon from '../../../assets/youtube_icon.svg';
 
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+
+  function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return windowDimensions;
+  }
+
 const HowItWorks = () => {
+    const { width } = useWindowDimensions();
+
     return (
         <>
             <h2 className="sm:text-4xl lg:text-5xl">How does it <span style={{ color: '#2778FD' }}>work?</span></h2>
             <Link to="" target="_blank"><span className="inline-block" style={{ color: '#2778FD' }}>Watch video</span> <YoutubeIcon className="inline-block"/> </Link>
             <div className="mt-8">
-                { window.innerWidth >= 1536 ? <LargeHowItWorksSteps className="mx-auto"/> : <MobileHowItWorksSteps className="mx-auto"/> }
+                { width >= 1536 ? <LargeHowItWorksSteps className="mx-auto"/> : <MobileHowItWorksSteps className="mx-auto"/> }
             </div>
         </>
     );
